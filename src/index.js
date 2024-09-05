6c70dd9e8c0874a7e37f5af3981ea2f7af36148a
Aditya <aditya2601.w@gmail.com>
2024-09-05 20:58:14 +0530

    Added errorhandler middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const connectDB = require("./db/index");
 const dotenv = require("dotenv");
-
+const errorHandler = require("./utils/errorHandler");
 dotenv.config({path:'./.env'});
+
+
 const app = express();
 
 connectDB();
+
+app.use(errorHandler)
